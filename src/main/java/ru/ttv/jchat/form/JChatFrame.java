c78e7a0d4eaae9c7ac747e093616b22fb422ff51
timteplych <timteplych@mail.ru>
2018-06-27 22:31:48 +0800

    chat area is readonly now, focus is staying at the message field

--- src/main/java/ru/ttv/jchat/form/JChatFrame.java
+++ src/main/java/ru/ttv/jchat/form/JChatFrame.java
@@ -4,6 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 /**
  * @author Timofey Teplykh
@@ -11,18 +15,61 @@ import java.awt.event.ActionListener;
  */
 
 public class JChatFrame extends JFrame {
+    private DataInputStream inputStream;
+    private DataOutputStream outputStream;
+    private Socket socket;
+    private boolean authorized;
+    //Chat text area
+    final JTextArea chatTextArea = new JTextArea();
+
     public JChatFrame(){
     }
 
     public void init(){
+        try{
+            socket = new Socket("localhost",8189);
+            inputStream = new DataInputStream(socket.getInputStream());
+            outputStream = new DataOutputStream(socket.getOutputStream());
+            setAuthorized(false);
+            Thread t = new Thread(()->{
+                try{
+                    while(true){
+                        String str = inputStream.readUTF();
+                        if(str.startsWith("/authok")){
+                            setAuthorized(true);
+                            break;
+                        }
+                        addChatMessageInMessageArea(str);
+                    }
+                    while(true){
+                        String str = inputStream.readUTF();
+                        if(str.equals("/end")){
+                            break;
+                        }
+                        addChatMessageInMessageArea(str);
+                    }
+                }catch(IOException e){
+                    e.printStackTrace();
+                }finally {
+                    try{
+                        socket.close();
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+                    setAuthorized(false);
+                }
+            });
+            t.setDaemon(true);
+            t.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
         setTitle("JChatTTV");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(300,50,450,600);
 
         setLayout(new BorderLayout());
 
-        //Chat text area
-        final JTextArea chatTextArea = new JTextArea();
         chatTextArea.setEditable(false);
 
         add(chatTextArea,BorderLayout.CENTER);
@@ -36,13 +83,17 @@ public class JChatFrame extends JFrame {
 
         ActionListener messageFieldActionListener = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                addChatMessageInMessageArea(chatTextArea,messageField);
+                addChatMessageInMessageArea(messageField.getText());
+                messageField.setText("");
+                messageField.requestFocusInWindow();
             }
         };
 
         ActionListener buttonSendActionListener = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                addChatMessageInMessageArea(chatTextArea,messageField);
+                addChatMessageInMessageArea(messageField.getText());
+                messageField.setText("");
+                messageField.requestFocusInWindow();
             }
         };
 
@@ -63,13 +114,16 @@ public class JChatFrame extends JFrame {
         messageField.requestFocusInWindow();
     }
 
-    void addChatMessageInMessageArea(JTextArea chatTextArea, JTextField messageField){
-        if(messageField.getText().equals("")){
+    void addChatMessageInMessageArea(String message){
+        if(message.equals("")){
             return;
         }
-        chatTextArea.append(messageField.getText());
+        chatTextArea.append(message);
         chatTextArea.append("\n");
-        messageField.setText("");
-        messageField.requestFocusInWindow();
+
+    }
+
+    private void setAuthorized(boolean authorized){
+        this.authorized = authorized;
     }
 }
