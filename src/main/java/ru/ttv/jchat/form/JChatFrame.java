70800211c079ab5d032891adf34e810960199862
timteplych <timteplych@mail.ru>
2018-07-01 10:49:47 +0800

    added consts

--- src/main/java/ru/ttv/jchat/form/JChatFrame.java
+++ src/main/java/ru/ttv/jchat/form/JChatFrame.java
@@ -19,6 +19,12 @@ public class JChatFrame extends JFrame {
     private DataOutputStream outputStream;
     private Socket socket;
     private boolean authorized;
+    private static final String SERVER_ADDRESS = "localhost";
+    private static final int SERVER_PORT = 8189;
+    private static final String APPLICATION_CAPTION = "JChatTTV";
+    private static final String AUTH_OK_STRING = "/authok";
+    private static final String END_STRING = "/end";
+    private static final String AUTH_STRING = "/auth";
 
     //Chat text area
     final JTextArea chatTextArea = new JTextArea();
@@ -28,18 +34,20 @@ public class JChatFrame extends JFrame {
 
     private void start(){
         try{
-            socket = new Socket("localhost",8189);
+            socket = new Socket(SERVER_ADDRESS,SERVER_PORT);
             inputStream = new DataInputStream(socket.getInputStream());
             outputStream = new DataOutputStream(socket.getOutputStream());
             setAuthorized(false);
-            Thread t = new Thread(()->{
+
+            //receiving messages thread
+            Thread receivingMessageThread = new Thread(()->{
                 try{
                     while(true){
                         String str = inputStream.readUTF();
-                        if(str.startsWith("/authok")){
+                        if(str.startsWith(AUTH_OK_STRING)){
                             String[] strs = str.split("\\s");
                             setAuthorized(true);
-                            setTitle("JChatTTV - "+strs[1]);
+                            setTitle(APPLICATION_CAPTION+" - "+strs[1]);
                             break;
                         }
                         //sendMessage(str);
@@ -47,7 +55,7 @@ public class JChatFrame extends JFrame {
                     }
                     while(true){
                         String str = inputStream.readUTF();
-                        if(str.equals("/end")){
+                        if(str.equals(END_STRING)){
                             break;
                         }
                         //sendMessage(str);
@@ -64,22 +72,22 @@ public class JChatFrame extends JFrame {
                     setAuthorized(false);
                 }
             });
-            t.setDaemon(true);
-            t.start();
+            receivingMessageThread.setDaemon(true);
+            receivingMessageThread.start();
         }catch (IOException e){
             e.printStackTrace();
         }
     }
 
     public void init(){
-        setTitle("JChatTTV");
+        setTitle(APPLICATION_CAPTION);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(300,50,450,600);
 
+        //setting border layout to main frame
         setLayout(new BorderLayout());
 
         chatTextArea.setEditable(false);
-
         add(chatTextArea,BorderLayout.CENTER);
 
         //Panel containg text enter elements
@@ -174,7 +182,7 @@ public class JChatFrame extends JFrame {
             start();
         }
         try {
-            outputStream.writeUTF("/auth "+loginField.getText()+" "+passwordField.getText());
+            outputStream.writeUTF(AUTH_STRING+" "+loginField.getText()+" "+passwordField.getText());
             loginField.setText("");
             passwordField.setText("");
         } catch (IOException e) {
